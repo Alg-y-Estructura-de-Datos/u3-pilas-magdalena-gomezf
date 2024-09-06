1e4f96c4c3ca586dc0e8c186fb3d1ab0e9349e4f
magdalenagomezf <2302577@ucc.edu.ar>
2024-09-06 20:32:56 -0300

    ejercicio 1

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
+#include "Pila/Pila.h"
+void funcionInvertida(Pila<char>pila){
 
+    while (!pila.esVacia()){
+        cout<< pila.pop();
+    }
+
+}
 int main() {
 
-    cout << "Ejercicio N° 1" << endl;
-
+    Pila<char> pila1;
+    char caracter;
+    cout << "INgrese una palabra. ingrese enter para terminar" << endl;
+    while (cin.get(caracter) && caracter != '\n') {
+        pila1.push(caracter);
+    }
+    funcionInvertida(pila1);
     return 0;
 }
