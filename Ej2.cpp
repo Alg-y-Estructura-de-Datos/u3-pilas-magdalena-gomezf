f6bea7ca9a6f9204887517106cae4bfa619f67a1
magdalenagomezf <2302577@ucc.edu.ar>
2024-09-06 20:53:23 -0300

    ejercicio 2

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
 using namespace std;
+#include "Pila/Pila.h"
+void intercambio(Pila<int> & pila1, int nuevo, int viejo){
+    Pila<int > aux;
+    int cambio;
 
+    while (!pila1.esVacia()){
+      cambio= pila1.pop();
+      aux.push(cambio);
+    }
+    while (!aux.esVacia()){
+        cambio=aux.pop();
+        if (viejo==cambio){
+            pila1.push(nuevo);
+
+        } else{
+            pila1.push(cambio);
+        }
+    }
+
+}
 int main() {
 
-    cout << "Ejercicio N° 2" << endl;
+    Pila<int> Pila1;
+    int tamanio, elemento, viejo, nuevo;
+    cout << "ingrese el tamanio de la pila" << endl;
+cin >> tamanio;
 
+cout << "Ingrese los " << tamanio << " numeros de la pila: ";
+    for (int i = 0;  i< tamanio; i++) {
+        cin >> elemento;
+        Pila1.push(elemento);
+    };
+    cout << "ingrese el numero nuevo";
+    cin >> nuevo;
+    cout << "ingrese el nuemro que desea remplazar";
+    cin >> viejo;
+
+    intercambio(Pila1,nuevo, viejo);
+
+    cout << "Pila despues del reemplazo: ";
+while (!Pila1.esVacia()) {
+       cout << Pila1.pop() << " ";
+  }
+    cout << endl;
     return 0;
 }
